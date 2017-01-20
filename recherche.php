4956e41dc9ebb8419003ce599bb6b4626305ddbd
houzz <houza95@hotmail.fr>
2017-01-20 22:54:31 +0100

    erreur resolu mais ne fonction pas encore

--- recherche.php
+++ recherche.php
@@ -2,20 +2,18 @@
 include('includes/connexion.inc.php');
 include('includes/haut.inc.php');
 
-
  if (isset($_POST['submit']) && !empty($_POST['submit']))
 	{
         $query = 'SELECT * FROM messages WHERE messages LIKE%'.$_POST['search'].'%' ;
         $message = $pdo->query($query);
-		
-        
+    var_dump($message);
 	while ($var=$message->fetch()) {
-	?>
-		<blockquote>
-			<?= $var['contenu'] ?>
-			<br><i><h6><b>par <?= $var['pseudo'] ?></b></h6>
+		
+?>		<blockquote>
+		<?= $var['contenu'] ?>
+		<br><i><h6><b>par <?= $var['pseudo'] ?></b></h6>
 		</blockquote>
-			<?
+	<?php 
 		}
-	}
-?>
+	}	
+	?>
